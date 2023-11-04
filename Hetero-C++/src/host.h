2b9ab77e9a95f9a13f9890b10dd0b71ab6e54816
Russel Arbore <rarbore2@illinois.edu>
2023-11-03 20:53:09 -0500

    Revert "ucihar dataset"
    This reverts commit d7e70fc70f20d881ebe6006cf42796f790f83879.

--- Hetero-C++/src/host.h
+++ Hetero-C++/src/host.h
@@ -13,25 +13,28 @@
 
 int train = 3; // number of training epochs (0 = inference)
 
-#define QUANT
-//#define ISOLET
-#define UCIHAR
-//#define MNIST
-
-#define Dhv				2048  //hypervectors length
-
-
-
-#ifdef ISOLET
-
 #define N_FEAT	        617 //feature per input (e.g., isolet: 617)
 #define N_CLASS		    26	//number of classes. (e.g., isolet: 26, ucihar 12)
+#define Dhv				2048  //hypervectors length
 #define N_SAMPLE 		6238 // FIXME: Make these parametesr variable.
+
+//#define N_TEST 			6238// Needs to be constant for templated funciton. 
 #define N_TEST 			1559  // Needs to be constant for templated funciton. 
 
-/* This is only for ISOLET */
-std::string rp_matrix_path = "../dataset/rp_matrix.bin";
-std::string rp_matrix_txt = "../dataset/rp_matrix.txt";
+#define COL				8 //number of columns of a matrix-vector multiplication window
+#define ROW				32 //number of rows of a matrix-vector multiplication window (32, 64, 128, 256, 512)
+
+#define PAD_			(N_FEAT & (COL - 1))
+
+#if PAD_ == 0
+	#define PAD 		0
+#else
+	#define PAD 		(COL - PAD_)
+#endif
+
+#define N_FEAT_PAD		(N_FEAT + PAD)	//feature per input (e.g., isolet: 624, ucihar 568)
+
+#define QUANT
 
 #ifdef QUANT
 // TODO: Add these to directory.
@@ -46,49 +49,10 @@ std::string X_train_path = "../dataset/isolet_nq_trainX.bin";
 std::string  y_train_path = "../dataset/isolet_nq_trainY.bin";
 std::string  X_test_path = "../dataset/isolet_nq_testX.bin";
 std::string  y_test_path = "../dataset/isolet_nq_testY.bin";
-#endif /* QUANT*/
-
-#endif /* ISOLET*/
-
-#ifdef MNIST
-
-#define N_CLASS		10	//number of centers. (e.g., isolet: 26,)
-#define N_FEAT			784	//feature per input (e.g., isolet: 617)
-#define N_SAMPLE 		10000
-#define N_TEST 			1
-std::string X_train_path = "../dataset/mnist_nq_trainX.bin";
-std::string  y_train_path = "../dataset/mnist_nq_trainY.bin";
-std::string  X_test_path = "../dataset/mnist_nq_testX.bin";
-std::string  y_test_path = "../dataset/mnist_nq_testY.bin";
 
 #endif
 
-#ifdef UCIHAR
+std::string rp_matrix_path = "../dataset/rp_matrix.bin";
 
-#define N_CLASS			6	//number of centers. (e.g., isolet: 26,)
-#define N_FEAT			561	//feature per input (e.g., isolet: 617)
-#define N_SAMPLE 		6213
-#define N_TEST 			1554
+std::string rp_matrix_txt = "../dataset/rp_matrix.txt";
 
-std::string X_train_path = "../dataset/ucihar_trainX.bin";
-std::string  y_train_path = "../dataset/ucihar_trainY.bin";
-std::string  X_test_path = "../dataset/ucihar_testX.bin";
-std::string  y_test_path = "../dataset/ucihar_testY.bin";
-
-#endif
-
-
-/* Padding for all .bin files. */
-
-#define COL				8 //number of columns of a matrix-vector multiplication window
-#define ROW				32 //number of rows of a matrix-vector multiplication window (32, 64, 128, 256, 512)
-
-#define PAD_			(N_FEAT & (COL - 1))
-
-#if PAD_ == 0
-	#define PAD 		0
-#else
-	#define PAD 		(COL - PAD_)
-#endif
-
-#define N_FEAT_PAD		(N_FEAT + PAD)	//feature per input (e.g., isolet: 624, ucihar 568)
