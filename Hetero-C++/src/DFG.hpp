186b84d3473e1ca6632f61876c2d9dc1b83716be
Russel Arbore <rarbore2@illinois.edu>
2024-03-19 16:15:31 -0500

    use cossim

--- Hetero-C++/src/DFG.hpp
+++ Hetero-C++/src/DFG.hpp
@@ -302,13 +302,16 @@ void __attribute__ ((noinline)) classification_node_inference(
     #ifdef HAMMING_DIST
     __hypervector__<K, hvtype> score =  __hetero_hdc_hamming_distance<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr);
     *scores_ptr = score;
-    //__hetero_hdc_sim_approx(score, 0, D, 1);
     #else
 #ifndef ACCEL
-    *norms_ptr = __hetero_hdc_l2norm<K, D, hvtype>(*classes_ptr);
-    *scores_ptr = __hetero_hdc_matmul<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr); 
-    *scores_ptr = __hetero_hdc_div<K, hvtype>(*scores_ptr, *norms_ptr);
-    *scores_ptr = __hetero_hdc_absolute_value<K, hvtype>(*scores_ptr);
+    //*norms_ptr = __hetero_hdc_l2norm<K, D, hvtype>(*classes_ptr);
+    //*scores_ptr = __hetero_hdc_matmul<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr); 
+    //*scores_ptr = __hetero_hdc_div<K, hvtype>(*scores_ptr, *norms_ptr);
+    //*scores_ptr = __hetero_hdc_absolute_value<K, hvtype>(*scores_ptr);
+    __hypervector__<K, hvtype> score =  __hetero_hdc_cossim<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr);
+    // UNCOMMENT for an approximated cosine similarity
+    //__hetero_hdc_sim_approx(score, 0, D / 4, 4);
+    *scores_ptr = score;
 #endif
     #endif
 
@@ -387,14 +390,15 @@ void classification_node_training_rest(/* Input Buffers: 2 */
     #ifdef HAMMING_DIST
     __hypervector__<K, hvtype> score =  __hetero_hdc_hamming_distance<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr);
     *scores_ptr = score;
-    //__hetero_hdc_sim_approx(score, 0, D, 1);
     #else
 
 #ifndef ACCEL
-    *norms_ptr = __hetero_hdc_l2norm<K, D, hvtype>(*classes_ptr);
-    *scores_ptr = __hetero_hdc_matmul<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr); 
-    *scores_ptr = __hetero_hdc_div<K, hvtype>(*scores_ptr, *norms_ptr);
-    *scores_ptr = __hetero_hdc_absolute_value<K, hvtype>(*scores_ptr);
+    //*norms_ptr = __hetero_hdc_l2norm<K, D, hvtype>(*classes_ptr);
+    //*scores_ptr = __hetero_hdc_matmul<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr); 
+    //*scores_ptr = __hetero_hdc_div<K, hvtype>(*scores_ptr, *norms_ptr);
+    //*scores_ptr = __hetero_hdc_absolute_value<K, hvtype>(*scores_ptr);
+    __hypervector__<K, hvtype> score =  __hetero_hdc_cossim<K, D, hvtype>(*encoded_hv_ptr, *classes_ptr);
+    *scores_ptr = score;
 #endif
     #endif
 
